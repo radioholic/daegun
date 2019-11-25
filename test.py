61fea5a5e4af136ab6c16e277bc7d2999a9b8b15
Soonchan Gwon <kd0bji@gmail.com>
2019-11-25 20:18:21 +0900

    Test Pycharm and GIThub

---
+++ test.py
@@ -0,0 +1,6 @@
+import time
+import random
+from tqdm import tqdm
+
+for i in tqdm(range(1,100)): # 이부분은 수정이 필요없습니다.
+    time.sleep(random.randrange(1,10)) # TODO 이 부분은 수정이 필요합니다.     
---
+++ test2.py
@@ -0,0 +1,5 @@
+for i in range(0,100):
+    try :
+        <웹크롤링>
+    except Exception as e:
+        print(e)
